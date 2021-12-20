80fa3f7cb88cfd46f9c513674c98e21281090a0e
robertrowe1013 <robertrowe1013@users.noreply.github.com>
2021-12-19 21:31:44 -0600

    populate player names in ui over network

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -1,23 +1,43 @@
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 using TMPro;
 
 public class MainGameManager : MonoBehaviourPunCallbacks
 {
     public GameObject playerPrefab;
     public TextMeshProUGUI roomName;
+    public TextMeshProUGUI[] displayNames;
+    PhotonView myPv;
     // Start is called before the first frame update
     void Start()
     {
+        myPv = this.GetComponent<PhotonView>();
         roomName.text = "Room Name: " + PlayerPrefs.GetString("RName");
         PhotonNetwork.Instantiate(playerPrefab.name, Vector3.zero, Quaternion.identity);
         PhotonNetwork.NickName = PlayerPrefs.GetString("PName");
-        Debug.Log(PhotonNetwork.NickName);
+        Debug.Log("Player Name: " + PhotonNetwork.NickName);
+        Debug.Log("Player List:");
+        Debug.Log("Count: " + PhotonNetwork.PlayerList.Length);
+        //Populate player names in UI
+        myPv.RPC("updateName", RpcTarget.All);
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    [PunRPC]
+    void updateName()
+    {
+        int i = 0;
+        foreach (Player player in PhotonNetwork.PlayerList)
+        {
+            displayNames[i].text = player.NickName;
+            i++;
+        }
+        i = 0;
     }
 }
